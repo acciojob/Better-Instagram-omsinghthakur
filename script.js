565c943427fc928f8ed1734f0f1c3e4b76f494d4
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-07-31 11:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+$(document).ready(function() {
+  $(".draggable").draggable({
+    revert: "invalid",
+    containment: "body",
+    cursor: "move",
+    zIndex: 100,
+  });
+
+  $(".draggable").droppable({
+    accept: ".draggable",
+    drop: function(event, ui) {
+      var draggableId = ui.draggable.attr("id");
+      var droppableId = $(this).attr("id");
+
+      if (draggableId !== droppableId) {
+        // Swap background images
+        var draggableBg = ui.draggable.css("background-image");
+        var droppableBg = $(this).css("background-image");
+
+        ui.draggable.css("background-image", droppableBg);
+        $(this).css("background-image", draggableBg);
+      }
+    },
+  });
+});
